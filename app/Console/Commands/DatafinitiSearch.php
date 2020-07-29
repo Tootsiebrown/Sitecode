70fd453c5f500550bd2beaa9648533123bb56eab
Bishop <bishop@oohology.com>
2020-07-29 12:15:19 +0000

    adds command for searching datafiniti for barcode

---
+++ app/Console/Commands/DatafinitiSearch.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Gateways\DatafinitiGateway;
+use Illuminate\Console\Command;
+
+class DatafinitiSearch extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'datafiniti:search {barcode}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    protected $gateway;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(DatafinitiGateway $gateway)
+    {
+        parent::__construct();
+
+        $this->gateway = $gateway;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        dd($this->gateway->barCodeSearch($this->argument('barcode')));
+    }
+}
