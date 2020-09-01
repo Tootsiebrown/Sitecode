bcda50649a63dd4cadd7c44d86f85d26b7e603dd
dbankes <davidbankes@gmail.com>
2020-09-01 19:49:44 -0400

    renamed factory

--- database/factories/ListingFactory.php
+++ database/factories/ListingFactory.php
@@ -3,7 +3,7 @@
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-$factory->define(App\Ad::class, function (Faker $faker) {
+$factory->define(App\Models\Listing::class, function (Faker $faker) {
     $title = $faker->words(3, true);
     return [
         'title' => $title,
