a0491fef07e630a573a6c5e37e5c44556518c507
dbankes <davidbankes@gmail.com>
2020-10-05 19:09:12 -0400

    a set of go_words that need hacked into search results.

--- app/Indexers/ListingsIndexer.php
+++ app/Indexers/ListingsIndexer.php
@@ -16,7 +16,11 @@ class ListingsIndexer implements \Wax\SiteSearch\Contracts\IndexerContract
     {
         $this->indexer = $indexer;
         $this->repo = $repo;
+        $this->goMap = config('wax.site-search.go_words');
     }
+
+
+
     public function crawl()
     {
         $items = $this->repo->getAll();
@@ -29,6 +33,15 @@ class ListingsIndexer implements \Wax\SiteSearch\Contracts\IndexerContract
                 . ' ' . $item->color
                 . ' ' . ($item->brand ? $item->brand->name : '')
                 . ' ' . $item->categories->pluck('name')->implode(' ');
+
+            if (!empty($this->goMap)) {
+                foreach ($this->goMap as $goTrigger => $goWord) {
+                    if (stripos($content, $goTrigger) !== false) {
+                        $content .= ' ' . $goWord;
+                        unset($this->goMap[$goTrigger]);
+                    }
+                }
+            }
 
             $page = array(
                 'module' => 'listings',
