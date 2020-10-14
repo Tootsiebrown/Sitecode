5f3770ee5780095e8160e9eec1634ffc7556b7d0
dbankes <davidbankes@gmail.com>
2020-10-13 23:02:32 -0400

    import class

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\ProcessEndedAuctions;
+use App\Console\Commands\ProcessExpiredOffers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
