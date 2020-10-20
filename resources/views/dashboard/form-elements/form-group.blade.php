8ff5e528c2445472e7b866c6317c92b141ab5598
dbankes <davidbankes@gmail.com>
2020-10-20 11:06:59 -0400

    improved error notation

--- resources/views/dashboard/form-elements/form-group.blade.php
+++ resources/views/dashboard/form-elements/form-group.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group" {{ $errors->has($name)? 'has-error':'' }}>
     <label for="{{ $name }}" class="col-sm-4 control-label">{{ $prettyTitle }}</label>
     <div class="col-sm-8">
         @include('dashboard.form-elements.inputs.' . $type, [
