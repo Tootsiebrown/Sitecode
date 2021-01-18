5940127ead4a3c5878861b6d5d39211afa1f904a
dbankes <davidbankes@gmail.com>
2021-01-18 08:28:01 -0500

    fix for unset variable

--- resources/views/dashboard/form-elements/inputs/select.blade.php
+++ resources/views/dashboard/form-elements/inputs/select.blade.php
@@ -1,4 +1,4 @@
-<select class="select2" name="{{ $name }}" @if ($inputAttributes) {!! $inputAttributes !!} @endif>
+<select class="select2" name="{{ $name }}" @if (isset($inputAttributes)) {!! $inputAttributes !!} @endif>
     @foreach($options as $optionValue => $optionLabel)
         <option value="{{ $optionValue }}" @if ($optionValue == old($name, $value)) {{ 'selected' }} @endif>
             {{ $optionLabel }}
