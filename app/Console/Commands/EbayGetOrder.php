eef7d4ad0eada7dcdff153720da2f88e9946e628
dbankes <davidbankes@gmail.com>
2021-01-21 15:46:54 -0500

    add a command to see an ebay order

---
+++ app/Console/Commands/EbayGetOrder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Ebay\Sdk;
+use App\Jobs\SyncEbayOrder;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class EbayGetOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ebay:get-order {orderId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync an order that was placed on ebay with the right inventory stuff locally';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param Sdk $ebay
+     * @return mixed
+     */
+    public function handle(Sdk $ebay): bool
+    {
+        $order = $ebay->getOrder($this->argument('orderId'));
+
+        dd($order);
+    }
+}
