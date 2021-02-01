2d322d7547a9e04113bcbe0e29d53b11301650be
dbankes <davidbankes@gmail.com>
2021-02-01 11:31:12 -0500

    stylin

--- app/Http/Controllers/Dashboard/HandlesEbayAspects.php
+++ app/Http/Controllers/Dashboard/HandlesEbayAspects.php
@@ -15,7 +15,7 @@ trait HandlesEbayAspects
             $rules['ebay_aspects.' . $requiredAspect] = 'required';
         }
 
-        foreach($request->input('ebay_aspects', []) as $maybeManualEntryAspect => $value) {
+        foreach ($request->input('ebay_aspects', []) as $maybeManualEntryAspect => $value) {
             if ($value === 'manual_entry') {
                 $rules['ebay_manual_aspects.' . $maybeManualEntryAspect] = 'required';
             }
